edb5b9b12ad1dbee43232891acc7794980f6ca9b
Bob <newUser@users.com>
2024-06-13 12:56:28 +0300

    first commit

---
+++ todo-tracer/src/main/java/com/tracer/todo_tracer/TodoTracerApplication.java
@@ -0,0 +1,13 @@
+package com.tracer.todo_tracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTracerApplication.class, args);
+	}
+
+}
---
+++ todo-tracer/src/test/java/com/tracer/todo_tracer/TodoTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracer.todo_tracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
